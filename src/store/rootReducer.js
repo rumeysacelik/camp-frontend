efb6e07fb19720b9df28071d2d0a70f74d908177
rumeysacelik <rmysclk61@icloud.com>
2021-06-17 00:50:35 +0300

    first commit

---
+++ src/store/rootReducer.js
@@ -0,0 +1,11 @@
+//tüm stateleri topladığımız yer
+
+import { combineReducers } from "redux";
+import cartReducer from "./reducers/cartReducer";
+
+
+const rootReducer = combineReducers({
+    cart : cartReducer,
+})
+
+export default rootReducer;
